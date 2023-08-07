262121dadb262366eef38068da7d66f9f6841652
mvaneijgen <mvaneijgen@gmail.com>
2023-08-07 10:51:48 +0200

    Tweaks

--- config.h
+++ config.h
@@ -22,4 +22,16 @@
 
 #define ENCODERS_PAD_A { D4 }
 #define ENCODERS_PAD_B { C6 }
-#define HOLD_ON_OTHER_KEY_PRESS
+// #define HOLD_ON_OTHER_KEY_PRESS
+#define DEBOUNCE 5
+// Configure the global tapping term (default: 200ms)
+#define TAPPING_TERM 175
+#define QUICK_TAP_TERM 0
+// #define RETRO_TAPPING
+
+// Prevent normal rollover on alphas from accidentally triggering mods.
+// #define IGNORE_MOD_TAP_INTERRUPT
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+// #define TAPPING_FORCE_HOLD
+// Apply the modifier on keys that are tapped during a short hold of a modtap
+#define PERMISSIVE_HOLD
